2e6bc536a0658709a0618c407b654850df5910e8
Leoncio97 <jonathanleoncio97@gmail.com>
2025-03-29 00:08:30 -0300

    feat: add mais funcionalidades para os componentes do React web.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
   return (
@@ -11,19 +11,30 @@ export const App = () => {
 }
 
 export const Login = () => {
+  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleEntrar = () => {
+      console.log(email);
+      console.log(password);
+    }
 
     return (
         <div>
             <form>
                 <label>
                     <span>Email:</span>
-                    <input/>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
                 </label>
                 
                 <label>
                     <span>Senha:</span>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
                 </label>
+
+                <button type="button" onClick={handleEntrar}>
+                  Entrar
+                </button>
             </form>
         </div>
         
